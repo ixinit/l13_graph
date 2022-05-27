25ba6baebc2264cc1cedf3fae3bd78b7306b354a
ixinit <asrtemmashenkoff20158@gmail.com>
2022-05-27 13:27:52 +0300

    Add camera and texture picker

--- l13_graph/Form1.Designer.cs
+++ l13_graph/Form1.Designer.cs
@@ -33,6 +33,20 @@ namespace l13_graph
             this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblX = new System.Windows.Forms.Label();
+            this.trackX = new System.Windows.Forms.TrackBar();
+            this.trackY = new System.Windows.Forms.TrackBar();
+            this.lblY = new System.Windows.Forms.Label();
+            this.trackZ = new System.Windows.Forms.TrackBar();
+            this.lblZ = new System.Windows.Forms.Label();
+            this.cbRotate = new System.Windows.Forms.CheckBox();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.openImage = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackZ)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // simpleOpenGlControl1
@@ -50,7 +64,7 @@ namespace l13_graph
             this.simpleOpenGlControl1.DepthBits = ((byte)(16));
             this.simpleOpenGlControl1.Location = new System.Drawing.Point(12, 12);
             this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
-            this.simpleOpenGlControl1.Size = new System.Drawing.Size(776, 426);
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(763, 538);
             this.simpleOpenGlControl1.StencilBits = ((byte)(0));
             this.simpleOpenGlControl1.TabIndex = 0;
             this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.OpenGlControlPaint);
@@ -61,14 +75,116 @@ namespace l13_graph
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.TimerTick);
             // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(14, 21);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(14, 13);
+            this.lblX.TabIndex = 1;
+            this.lblX.Text = "X";
+            // 
+            // trackX
+            // 
+            this.trackX.LargeChange = 2;
+            this.trackX.Location = new System.Drawing.Point(17, 37);
+            this.trackX.Name = "trackX";
+            this.trackX.Size = new System.Drawing.Size(147, 45);
+            this.trackX.TabIndex = 2;
+            this.trackX.Scroll += new System.EventHandler(this.trackX_Scroll);
+            // 
+            // trackY
+            // 
+            this.trackY.LargeChange = 2;
+            this.trackY.Location = new System.Drawing.Point(17, 116);
+            this.trackY.Name = "trackY";
+            this.trackY.Size = new System.Drawing.Size(147, 45);
+            this.trackY.TabIndex = 4;
+            this.trackY.Scroll += new System.EventHandler(this.trackY_Scroll);
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(14, 100);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(14, 13);
+            this.lblY.TabIndex = 3;
+            this.lblY.Text = "Y";
+            // 
+            // trackZ
+            // 
+            this.trackZ.LargeChange = 2;
+            this.trackZ.Location = new System.Drawing.Point(17, 187);
+            this.trackZ.Name = "trackZ";
+            this.trackZ.Size = new System.Drawing.Size(147, 45);
+            this.trackZ.TabIndex = 6;
+            this.trackZ.Scroll += new System.EventHandler(this.trackZ_Scroll);
+            // 
+            // lblZ
+            // 
+            this.lblZ.AutoSize = true;
+            this.lblZ.Location = new System.Drawing.Point(14, 171);
+            this.lblZ.Name = "lblZ";
+            this.lblZ.Size = new System.Drawing.Size(14, 13);
+            this.lblZ.TabIndex = 5;
+            this.lblZ.Text = "Z";
+            // 
+            // cbRotate
+            // 
+            this.cbRotate.AutoSize = true;
+            this.cbRotate.Location = new System.Drawing.Point(26, 255);
+            this.cbRotate.Name = "cbRotate";
+            this.cbRotate.Size = new System.Drawing.Size(71, 17);
+            this.cbRotate.TabIndex = 7;
+            this.cbRotate.Text = "Вращать";
+            this.cbRotate.UseVisualStyleBackColor = true;
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(26, 293);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(89, 23);
+            this.btnImage.TabIndex = 8;
+            this.btnImage.Text = "Изображение";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.btnImage_Click);
+            // 
+            // openImage
+            // 
+            this.openImage.FileName = "openImage";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.trackZ);
+            this.panel1.Controls.Add(this.btnImage);
+            this.panel1.Controls.Add(this.lblX);
+            this.panel1.Controls.Add(this.cbRotate);
+            this.panel1.Controls.Add(this.trackX);
+            this.panel1.Controls.Add(this.lblY);
+            this.panel1.Controls.Add(this.lblZ);
+            this.panel1.Controls.Add(this.trackY);
+            this.panel1.Location = new System.Drawing.Point(796, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 538);
+            this.panel1.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1002, 562);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.simpleOpenGlControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackZ)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -78,6 +194,16 @@ namespace l13_graph
         private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.TrackBar trackX;
+        private System.Windows.Forms.TrackBar trackY;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.TrackBar trackZ;
+        private System.Windows.Forms.Label lblZ;
+        private System.Windows.Forms.CheckBox cbRotate;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.OpenFileDialog openImage;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- l13_graph/Form1.cs
+++ l13_graph/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.DevIl;
 
 namespace l13_graph
 {
@@ -20,16 +22,29 @@ namespace l13_graph
         int fragment = 0;  // идентификатор фрагментного шейдера
         int program = 0;   // идентификатор программного объекта
 
+        double tX, tY, tZ;
+        private int imageId;
+        private uint mGlTextureObject;
+        private bool textureIsLoad;
+
         public Form1()
         {
             InitializeComponent();
             simpleOpenGlControl1.InitializeContexts();
             Init();
             Resize(simpleOpenGlControl1.Width, simpleOpenGlControl1.Height);
-            LoadShadrers();
+            //LoadShadrers();
         }
         private static bool Init()
         {
+            Glut.glutInit();
+
+
+            // инициализация библиотеки openIL
+            Il.ilInit();
+            Il.ilEnable(Il.IL_ORIGIN_SET);
+
+
             Gl.glEnable(Gl.GL_COLOR_MATERIAL);
             Gl.glShadeModel(Gl.GL_SMOOTH);
 
@@ -63,8 +78,9 @@ namespace l13_graph
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
             Gl.glLoadIdentity();
 
-            Gl.glTranslated(1.0, -1.0, 1.0);
-            Gl.glRotated(angle, 0.0, 1.0, 0.0);
+            Gl.glTranslated(tX, tY, tZ);
+            if(cbRotate.Checked)
+                Gl.glRotated(angle, 0.0, 1.0, 0.0);
 
             Gl.glBegin(Gl.GL_LINES);
                 Gl.glColor3d(1.0, 0.0, 0.0);
@@ -84,41 +100,46 @@ namespace l13_graph
                 Gl.glVertex3d(1, 1, 1);
             Gl.glEnd();
 
-
-            /*Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-            Gl.glLoadIdentity();
-
-            Gl.glTranslated(0.0, -0.5, -4.0);
-            Gl.glRotated(angle, 0.0, 1.0, 0.0);
-
-            Gl.glColor3d(0.0, 1.0, 0.0);
             Gl.glBegin(Gl.GL_QUADS);
-            Gl.glVertex3d(-1, -0.5, -1);
-            Gl.glVertex3d(1, -0.5, -1);
-            Gl.glVertex3d(1, -0.5, 1);
-           *//* Gl.glVertex3d(-1, -0.5, 1);
-            Gl.glVertex3d(2, 1.5, 2);*//*
-            Gl.glVertex3d(-4, 1.5, 2);
+            //Texture2D.LoadFromImage(string fileName)
+                Gl.glColor3d(0.5, 0.5, 0.0);
+                Gl.glVertex3d(-1, tY, -1);
+                Gl.glVertex3d(1, tY, -1);
+                Gl.glVertex3d(1, tY, 1);
+                Gl.glVertex3d(-1, tY, 1);
             Gl.glEnd();
+            
+            if (textureIsLoad)
+            {
+                // включаем режим текстурирования
+                Gl.glEnable(Gl.GL_TEXTURE_2D);
+                // включаем режим текстурирования , указывая индификатор mGlTextureObject
+                Gl.glBindTexture(Gl.GL_TEXTURE_2D, mGlTextureObject);
 
-            Gl.glTranslated(0.0, 1.0, 0.0);
+                
+                Glu.GLUquadric q = Glu.gluNewQuadric();
+                Glu.gluSphere(q, 0.2, 50, 50);
+                Glu.gluDeleteQuadric(q);
 
-            Gl.glUseProgram(program);
+                /*// отрисовываем полигон
+                Gl.glBegin(Gl.GL_QUADS);
 
-            Gl.glColor3d(1.0, 0.0, 1.0);
-            Glu.GLUquadric q = Glu.gluNewQuadric();
-            Glu.gluSphere(q, 0.5, 50, 50);
-            Glu.gluDeleteQuadric(q);
+                // указываем поочередно вершины и текстурные координаты
+                Gl.glVertex3d(1, 1, 0);
+                Gl.glTexCoord2f(0, 0);
+                Gl.glVertex3d(1, 0, 0);
+                Gl.glTexCoord2f(1, 0);
+                Gl.glVertex3d(0, 0, 0);
+                Gl.glTexCoord2f(1, 1);
+                Gl.glVertex3d(0, 1, 0);
+                Gl.glTexCoord2f(0, 1);
 
-            Gl.glUseProgram(0);
+                // завершаем отрисовку
+                Gl.glEnd();*/
 
-
-            *//*Gl.glColor3d(1.0, 0.2, 0.5);
-            Glu.GLUquadric q = Glu.gluNewQuadric();
-            Glu.gluSphere(q, 0.5, 50, 50);
-            Glu.gluDeleteQuadric(q);*//*
-
-            Gl.glFlush();*/
+                // отключаем режим текстурирования
+                Gl.glDisable(Gl.GL_TEXTURE_2D);
+            }
         }
         private void TimerTick(object sender, EventArgs e)
         {
@@ -156,6 +177,127 @@ namespace l13_graph
             sr = null;
             GC.Collect();
 
+        }
+
+        private void trackX_Scroll(object sender, EventArgs e)
+        {
+            tX = (double)trackX.Value / 1000;
+            lblX.Text = "X: " + tX.ToString();
+        }
+        private void trackY_Scroll(object sender, EventArgs e)
+        {
+            tY = (double)trackY.Value / 1000;
+            lblY.Text = "Y: " + tY.ToString();
+        }
+        private void trackZ_Scroll(object sender, EventArgs e)
+        {
+            tZ = (double)trackZ.Value / 1000;
+            lblZ.Text = "Z: " + tZ.ToString();
+        }
+
+        private void btnImage_Click(object sender, EventArgs e)
+        {
+            // открываем окно выбора файла
+            DialogResult res = openImage.ShowDialog();
+
+            // есл файл выбран - и возвращен результат OK
+            if (res == DialogResult.OK)
+            {
+                // создаем изображение с индификатором imageId
+                Il.ilGenImages(1, out imageId);
+                // делаем изображение текущим
+                Il.ilBindImage(imageId);
+
+                // адрес изображения полученный с помощью окна выбра файла
+                string url = openImage.FileName;
+
+                // пробуем загрузить изображение
+                if (Il.ilLoadImage(url))
+                {
+                    // если загрузка прошла успешно
+                    // сохраняем размеры изображения
+                    int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                    int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
+
+                    // определяем число бит на пиксель
+                    int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+
+                    switch (bitspp) // в зависимости оп полученного результата
+                    {
+                        // создаем текстуру используя режим GL_RGB или GL_RGBA
+                        case 24:
+                            mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
+                            break;
+                        case 32:
+                            mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
+                            break;
+                    }
+
+                    // активируем флаг, сигнализирующий загрузку текстуры
+                    textureIsLoad = true;
+                    // очищаем память
+                    Il.ilDeleteImages(1, ref imageId);
+
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackX.Minimum = -1000;
+            trackX.Maximum = 1000;
+            tX = 0;
+            trackX.Value = 0;
+
+            trackY.Minimum = -1000;
+            trackY.Maximum = 1000;
+            tY = -0.2;
+            trackY.Value = -200;
+
+            trackZ.Minimum = -1000;
+            trackZ.Maximum = 1000;
+            tZ = -0.6;
+            trackZ.Value = -600;
+        }
+
+        // создание текстуры в панями openGL
+        private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
+        {
+            // индетефекатор текстурного объекта
+            uint texObject;
+
+            // генерируем текстурный объект
+            Gl.glGenTextures(1, out texObject);
+
+            // устанавливаем режим упаковки пикселей
+            Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+
+            // создаем привязку к только что созданной текстуре
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
+
+            // устанавливаем режим фильтрации и повторения текстуры
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
+
+            // создаем RGB или RGBA текстуру
+            switch (Format)
+            {
+                case Gl.GL_RGB:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+
+                case Gl.GL_RGBA:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+            }
+
+            // возвращаем индетефекатор текстурного объекта
+
+            return texObject;
         }
     }
 }
